4c2edea09e88b89e29f275e0755a2c18ec96711a
Thays Omine <thaysomine@gmail.com>
2022-03-18 09:34:47 -0300

    Flashcards added dynamically

--- src/Deck.js
+++ src/Deck.js
@@ -1,33 +1,13 @@
-//import { useState } from "react";
+import Main from "./Main";
+import Footer from "./Footer";
+import Header from "./Header";
 
 export default function Deck() {
     return (
         <> 
-            <header>
-                <img src="assets/logo.png" alt="logo"/>
-                <p>ZapRecall</p>
-            </header>
-            <main className="deck">
-                <div className="question">
-                    <p>Pergunta 1</p>
-                    <img src="assets/seta.svg" alt="seta"/>
-                </div>
-                <div className="question">
-                    <p>Pergunta 1</p>
-                    <img src="assets/seta.svg" alt="seta"/>
-                </div>
-                <div className="question">
-                    <p>Pergunta 1</p>
-                    <img src="assets/seta.svg" alt="seta"/>
-                </div>
-                <div className="question">
-                    <p>Pergunta 1</p>
-                    <img src="assets/seta.svg" alt="seta"/>
-                </div>
-            </main>
-            <footer>
-                <p>0/4 CONCLUÍDOS</p>
-            </footer>
+            <Header />
+            <Main />
+            <Footer />
         </>
     )
 }
---
+++ src/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="assets/logo.png" alt="logo"/>
+            <p>ZapRecall</p>
+        </header>
+    )
+}
